5c3ee24ababa17af78b3f05a17c365e07007a943
Jordi Coscolla <kozko2001@gmail.com>
2024-08-09 10:04:40 +0200

    init

---
+++ README.md
@@ -0,0 +1,42 @@
+# Logseq Nix Builder
+
+This is a utility to help build any version of logseq from source code
+using nix shell
+
+
+Steps:
+1. make sure you have nix installed
+```
+which nix
+```
+
+should return a specific path, to install follow nix documentation here https://nix.dev/install-nix.html
+
+2. enter the nix-shell
+
+```
+nix-shell
+```
+
+3. download logseq repository 
+```
+git clone https://github.com/logseq/logseq.git logseq
+cd logseq
+```
+
+4. choose the branch you want to compile
+
+```
+git checkout feat/db
+```
+
+5. execute the build command
+
+```
+build
+```
+
+## How does it work
+
+- Creates a nix shell with all the requirements from the github action workflows files
+- executes the release commands
---
+++ shell.nix
@@ -0,0 +1,39 @@
+{ pkgs ? import <nixpkgs> {
+    overlays = [
+      (import ./clojure-overlay.nix)
+    ];
+} }:
+
+let
+  nodejs = pkgs.nodejs-18_x;
+  nodePackages = pkgs.nodePackages // {
+    inherit (nodejs) nodejs;
+    gulp = pkgs.nodePackages.gulp.override { inherit nodejs; };
+    yarn = pkgs.nodePackages.yarn.override { inherit nodejs; };
+    node-gyp = pkgs.nodePackages.node-gyp.override { inherit nodejs; };
+  };
+  macosDeps = if pkgs.stdenv.isDarwin then [
+    pkgs.darwin.apple_sdk.frameworks.Foundation
+    pkgs.darwin.apple_sdk.frameworks.AppKit
+  ] else [];
+in
+pkgs.mkShell {
+  buildInputs = [
+      nodejs
+      pkgs.zulu11
+      pkgs.clojure
+      nodePackages.gulp
+      nodePackages.yarn
+      nodePackages.node-gyp
+    ] ++ macosDeps;
+  shellHook = ''
+    export npm_config_python=${pkgs.python3}/bin/python3
+    export PATH=${pkgs.gcc}/bin:${pkgs.gnumake}/bin:$PATH
+
+    function build() {
+      yarn install && gulp build && yarn cljs:release-electron;
+      yarn run release-electron
+
+    }
+  '';
+}
